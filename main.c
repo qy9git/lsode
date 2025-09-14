e3bc300edc3c73240f7d86ac31c50f1b56cecd8f
qy9git <mehmetsarrac5@gmail.com>
2025-09-14 20:53:45 +0200

    a new sane approach

--- main.c
+++ main.c
@@ -1,63 +1,17 @@
-#include <dirent.h>
+#define _GNU_SOURCE
+#include <fcntl.h>
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-#define INIT_SIZE 256
-
-struct StringBuilder {
-    char **data;
-    size_t capacity;
-    size_t item;
-};
-
-void init(struct StringBuilder *sb) {
-    sb->data = (char **)malloc(256 * sizeof(char *));
-    
-    if(!sb->data) {
-        puts("Error while allocating");
-        return;
-    }
-
-    sb->item = 0;
-    sb->capacity = INIT_SIZE;
-}
-
-void append(struct StringBuilder *sb, const char *data) {
-    if (sb->data == NULL) init(sb);
-
-    if(sb->item == sb->capacity) {
-        sb->capacity *= 2;
-        sb->data = realloc(sb->data, sb->capacity * sizeof(char *));
-    }
-
-    sb->data[sb->item++] = data;
-}
-
-static char **getString(struct StringBuilder *sb, size_t index) {
-    if(index < array->size) {
-        return array->data[index];
-    } else {
-        puts("ERROR: out of bounds");
-        return NULL;
-    }
-}
+#include <sys/stat.h>
 
 int main(int argc, char*argv[]){
-   // TODO: Parse le ./
-   for (int i = 1 ; i < argc; ++i) {
-       DIR *dir;
-       char *file_path = argv[i];
-       if ((dir = opendir(file_path)) == NULL) { // man opendir
-           puts("ERROR: Could not open directory at the current dir");
-           return 1;
-       }
-       struct dirent *de;
-       while ((de = readdir(dir)) != NULL) { // man 2 readdir
-           if(strcmp(de->d_name,"..") == 0 || strcmp(de->d_name,".") == 0) {
-               continue;
-           }
-       }
+   for(int i=1;i<argc;++i){
+       int fd=open(argv[i],O_PATH);
+       if(fd<0)
+           continue;
+       struct stat st;
+       if(fstat(fd, &st))
+           continue;
+       printf("");
    }
    return 0;
 }
