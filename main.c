e5711515fc437dfb123475d1f81f681c61330433
qy9git <mehmetsarrac5@gmail.com>
2025-09-15 16:34:11 +0200

    added file types and better permissions

--- main.c
+++ main.c
@@ -6,8 +6,39 @@
 
 static void print_info(struct stat st,const char *name){
     struct tm tm=*localtime(&st.st_mtime);
-    printf("%04o %d-%02d-%02d %02d:%02d:%02d %ld %s\n",
+    char * type[7]={//man 7 inode
+        "FIFO            ","character device",
+        "directory       ","block device    ",
+        "regular file    ","symbolic link   ",
+        "socket          "
+    };
+    char perm[12]="--- --- ---";
+    if(st.st_mode & S_IRUSR)
+        perm[0]='r';
+    if(st.st_mode & S_IWUSR)
+        perm[1]='w';
+    if(st.st_mode & S_IXUSR)
+        perm[2]='x';
+
+    if(st.st_mode & S_IRGRP)
+        perm[4]='r';
+    if(st.st_mode & S_IWGRP)
+        perm[5]='w';
+    if(st.st_mode & S_IXGRP)
+        perm[6]='x';
+
+    if(st.st_mode & S_IROTH)
+        perm[8]='r';
+    if(st.st_mode & S_IWOTH)
+        perm[9]='w';
+    if(st.st_mode & S_IXOTH)
+        perm[10]='x';
+
+    printf("%s | %04o %s | %d-%02d-%02d %02d:%02d:%02d | %010ld bytes | %s\n",
+           type[st.st_mode>>13],
+
            st.st_mode & 0777,
+           perm,
 
            tm.tm_year+1900,
            tm.tm_mon+1,
